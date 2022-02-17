a1bae05a43187cba23db59f3e2cbcca1b8abd34c
TenForBenJamin <sibinsurendran49@gmail.com>
2022-02-17 01:15:14 +0100

    lalalala

---
+++ kiran/src/main/java/meri/kiran/App.java
@@ -0,0 +1,13 @@
+package meri.kiran;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
